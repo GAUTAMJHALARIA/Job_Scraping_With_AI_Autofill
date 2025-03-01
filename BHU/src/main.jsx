1729b75a8084347cef6a37f963e5bcfa8177c9b8
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2025-03-01 09:37:48 +0530

    joblist api and css

--- BHU/src/main.jsx
+++ BHU/src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MemoryRouter  } from "react-router-dom"; // Use MemoryRouter for Chrome Extension
+import { BrowserRouter  } from "react-router-dom"; // Use MemoryRouter for Chrome Extension
 import App from "./App.jsx";
 import AuthProvider from "./Components/context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <MemoryRouter> {/* Wrap the app in MemoryRouter */}
+      <BrowserRouter> {/* Wrap the app in MemoryRouter */}
         <App />
-      </MemoryRouter>
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
